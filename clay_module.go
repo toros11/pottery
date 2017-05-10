668f8a5ce598dc9ea09d156aa2b8b2d724fa473a
qb0C80aE <qb0C80aE@gmail.com>
2017-05-10 23:53:08 +0900

    Correspond redesign and rearrangement of Clay on 'feature/rearrange-tags'

---
+++ clay_module.go
@@ -0,0 +1,8 @@
+package pottery
+
+import (
+	_ "github.com/qb0C80aE/pottery/controllers" // Install Pottery controller by importing
+	_ "github.com/qb0C80aE/pottery/logics"      // Install Pottery logic by importing
+	_ "github.com/qb0C80aE/pottery/models"      // Install Pottery model by importing
+	_ "github.com/qb0C80aE/pottery/ui"          // Install Pottery ui by importing
+)
--- debug/generate.go
+++ debug/generate.go
@@ -1,6 +1,6 @@
 // +build debug
-// exec 'go generate -tags=debug ./debug/...' to generate bindata for debugging
+// execute 'go generate -tags=debug debug/generate.go' or 'go generate -tags=debug ./...' to generate bindata.go for debugging manually
 
-package debug
+package main
 
 //go:generate go-bindata -debug=true -pkg ui -o ../ui/bindata.go -prefix=../ ../ui/files/... ../ui/templates/...
